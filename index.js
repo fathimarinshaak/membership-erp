d16c96b27495dba9d6366d6f2cd93b659fae1aca
nadiyakm <kmnadiya003@gmail.com>
2026-02-02 13:38:07 +0530

    db connected

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
+const connectDB = require('./config/db');
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, async () => {
-    console.log(` App started on port ${port}`);
+	await connectDB();
+	console.log(` App started on port ${port}`);
 });
