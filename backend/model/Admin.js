9fc93952d4bedb3cea2b8be1df201200b4933264
Fathima Rinsha A K <fathimaofficial456@gmail.com>
2026-02-03 14:10:09 +0530

    react installed

--- backend/model/Admin.js
+++ backend/model/Admin.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 const adminSchema = new mongoose.Schema({
     name: {
@@ -10,12 +11,34 @@ const adminSchema = new mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        trim:true
+        trim: true
     },
     password: {
         type: String,
         required: true
     }
 }, { timestamps: true });
+
+adminSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+adminSchema.methods.validatePassword = async function (adminpassword) {
+    return await bcrypt.compare(adminpassword, this.password)
+}
+
+adminSchema.methods.getjwt = function () {
+    const token = jwt.sign({
+        Id: this._id,
+        email: this.email,
+        role: 'admin'
+    }, process.env.jwtsecret, { expiresIn: '1hr' })
+    return token
+}
+
 
 module.exports = mongoose.model('Admin', adminSchema);
