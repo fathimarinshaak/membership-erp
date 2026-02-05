3ab30705e983ee62ec677aa4fefc634942440ac6
Mahira Minna <mahiraminna728@gmail.com>
2026-02-05 12:26:11 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages/admin/AdminLogin";
 import { payNow } from "./pay";
-import{BrouserRouter,Routes,Route} from "react-router"
-import login from "./pages/admin/login"
-
-
 
 export default function App() {
   return (
-    <div className="container">
-      <h1>Razorpay Payment</h1>
-      <button onClick={payNow}>Razor Pay</button>
-      <button>Cash Free</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* AdminLogin */}
+        <Route path="/admin/login" element={<Login />} />
+
+        <Route
+          path="/"
+          element={
+            <div className="container mx-auto p-6">
+              <h1 className="text-2xl font-bold mb-4">
+                Razorpay Payment
+              </h1>
+
+              <button
+                onClick={payNow}
+                className="bg-blue-600 text-white px-4 py-2 rounded mr-3"
+              >
+                Razorpay
+              </button>
+
+              <button className="bg-green-600 text-white px-4 py-2 rounded">
+                Cashfree
+              </button>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
