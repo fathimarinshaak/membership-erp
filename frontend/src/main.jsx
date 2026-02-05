762b0cb5798f11ef8e4306b3b867ce89647a947e
Hana <hanah4ris@gmail.com>
2026-02-05 12:20:13 +0530

    addViewPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AddMember from './pages/admin/addMember.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
