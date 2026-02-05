1d7d8b926bbafce6f3159c0289d3a4afb7f7f15c
Mahira Minna <mahiraminna728@gmail.com>
2026-02-05 16:44:53 +0530

    sidebar updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "./pages/admin/AdminLogin";
-import { payNow } from "./pay";
 import AddMember from "./pages/admin/addMember";
 import ViewMember from "./pages/admin/ViewMembers";
+import Home from "./pages/admin/Home";
+import AdminLayout from "./layouts/AdminLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* AdminLogin */}
         <Route path="/admin/login" element={<Login />} />
 
-        <Route
-          path="/"
-          element={
-            <div className="container mx-auto p-6">
-              <h1 className="text-2xl font-bold mb-4">
-                Razorpay Payment
-              </h1>
-
-              <button
-                onClick={payNow}
-                className="bg-blue-600 text-white px-4 py-2 rounded mr-3"
-              >
-                Razorpay
-              </button>
-
-              <button className="bg-green-600 text-white px-4 py-2 rounded">
-                Cashfree
-              </button>
-            </div>
-          }
-        />
-          <Route path="/admin/addMember" element={<AddMember />} />
-        <Route path="/admin/viewMember" element={<ViewMember />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Home />} />
+          <Route path="addMember" element={<AddMember />} />
+          <Route path="viewMember" element={<ViewMember />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-    
   );
 }
